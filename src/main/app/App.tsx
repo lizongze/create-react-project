a692f5c4dbfb7421271323f6cb2cecd7a11a5653
lizongze <leo.ze@qq.com>
2025-05-18 20:34:53 +0800

    refactor: .

--- src/main/app/App.tsx
+++ src/main/app/App.tsx
@@ -1,22 +1,20 @@
-import React, { Suspense } from 'react';
+import { Suspense } from 'react';
 import { Provider as MobxProvider } from 'mobx-react';
-import AppRouter from './AppRouter';
+import { AppRouter } from './AppRouter';
 
 const globalMobxStore = {};
 
-export class App extends React.PureComponent<{ store; hide?: boolean }> {
-  render() {
-    const { store: curStore, hide } = this.props;
-    const store = curStore || globalMobxStore;
-    // N.B: hide for prefetch AppStartUp chunk after LoginInterceptor mounted
-    return (
-      <MobxProvider store={store}>
-        <Suspense>
-          <AppRouter store={store} />
-        </Suspense>
-      </MobxProvider>
-    );
-  }
-}
+export const App = (props: { store; hide?: boolean }) => {
+  const { store: curStore, hide } = props;
+  const store = curStore || globalMobxStore;
+  // N.B: hide for prefetch AppStartUp chunk after LoginInterceptor mounted
+  return (
+    <MobxProvider store={store}>
+      <Suspense>
+        <AppRouter store={store} />
+      </Suspense>
+    </MobxProvider>
+  );
+};
 
 export default App;
