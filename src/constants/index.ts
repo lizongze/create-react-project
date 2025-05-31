06dd9ad6316fcabc7567c8f4051cce6927209a6e
lizongze <leo.ze@qq.com>
2025-05-31 12:37:04 +0800

    fix: change timeout from 5 into 15

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,8 @@
 export const DEBUG = false;
 
 export const OpenAiKey =
-  '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';
+  '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';
+  // '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';
 // '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';
 // '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';
 
